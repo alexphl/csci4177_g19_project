554011650273df29c6bb033859728a633e292453
Liam Osler <osler.liam@gmail.com>
2023-02-02 16:29:25 -0400

    Add navigation to sidebar drawer and navbar

---
+++ components/Navbar.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar({ open, onOpen }) {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <AppBar position="static">
+                <Toolbar>
+                <IconButton
+                    onClick={() => onOpen(!open)}
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                <MenuIcon
+                />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Coffee
+                </Typography>
+                <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+      );
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,54 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Navbar from '@components/Navbar';
+import Sidebar from '@components/Sidebar';
+
+
+import React, { useState } from "react";
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#070405',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#0066ff',
+      main: '#0044ff',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+    // Provide every color token (light, main, dark, and contrastText) when using
+    // custom colors for props in Material UI's components.
+    // Then you will be able to use it like this: `<Button color="custom">`
+    // (For TypeScript, you need to add module augmentation for the `custom` value)
+    custom: {
+      light: '#ffa726',
+      main: '#f57c00',
+      dark: '#ef6c00',
+      contrastText: 'rgba(0, 0, 0, 0.87)',
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+});
+
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+        <Navbar open={open} onOpen={setOpen} />
+        <Sidebar open={open} onOpen={setOpen}/>
+    </ThemeProvider>
   )
 }
