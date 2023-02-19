bee64dc6381f72f57f62329d48e498357cf1c9bc
Olexiy Prokhvatylo <olexiyprokhvatylo@gmail.com>
2023-02-18 22:17:13 -0400

    Prep for registration merge

--- frontend/src/app/dashboard/layout.js
+++ frontend/src/app/dashboard/layout.js
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createContext, useReducer } from "react";
 
 const NavDesktop = dynamic(() => import("./NavDesktop"));
 
@@ -11,17 +12,40 @@ const themeMUI = createTheme({
   },
 });
 
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        user: action.payload,
+      };
+    case "LOGOUT_USER":
+      return {
+        user: { loggedIn: false, email: null },
+      };
+    default:
+      return state;
+  }
+};
+
 export default function Home({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }) {
+  const userContext = createContext();
+
+  const [userState, dispatchUser] = useReducer(userReducer, {
+    // initial state
+    user: { loggedIn: false, email: null },
+  });
 
   return (
     <>
       <ThemeProvider theme={themeMUI}>
-        <NavDesktop />
-        <div className="text-neutral-100 sm:ml-20">{children}</div>
+        <userContext.Provider value={{ ...userState, dispatchUser }}>
+          <NavDesktop />
+          <div className="text-neutral-100 sm:ml-20">{children}</div>
+        </userContext.Provider>
       </ThemeProvider>
     </>
   );
