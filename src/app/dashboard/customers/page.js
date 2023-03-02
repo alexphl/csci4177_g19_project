c4ef00a207fe0970249985f18374eae0afeb4fdd
Liam Osler <osler.liam@gmail.com>
2023-03-01 22:07:16 -0400

    add customer filter options

--- src/app/dashboard/customers/page.js
+++ src/app/dashboard/customers/page.js
@@ -4,6 +4,18 @@ import { useQuery } from "@tanstack/react-query";
 
 import Grid from "@mui/material/Grid";
 import CircularProgress from "@mui/material/CircularProgress";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 import CustomerCard from "../CustomerCard";
 
@@ -18,6 +30,31 @@ export default function CustomerTable() {
 
   return (
     <div className="container my-20 max-w-6xl px-10">
+      <h1 className="text-2xl leading-relaxed text-neutral-400">
+        View our <br />
+        <strong className="text-4xl text-white">CUSTOMERS</strong>
+      </h1>
+      
+      <h1 className="text-2xl leading-relaxed text-neutral-400 mt-4 mb-2">
+        Filter:
+      </h1>
+
+      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+
+        <div>
+          <TextField
+            label="By Username"
+            id="outlined-start-adornment"
+            sx={{ mb: 4, mr: 2}}
+          />
+          <TextField
+            label="By Email Address"
+            id="outlined-start-adornment"
+            sx={{ mb: 4}}
+          />
+        </div>
+
+      </Box>
       <Grid container spacing={2}>
         {isSuccess &&
           data &&
