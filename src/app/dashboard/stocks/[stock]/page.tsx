2a39c9d71904e0c7f516faa3f325281c93057db3
Olexiy Prokhvatylo <olexiyprokhvatylo@gmail.com>
2023-03-04 16:43:44 -0400

    Stocks: remove Arthur's news component

--- src/app/dashboard/stocks/[stock]/page.tsx
+++ src/app/dashboard/stocks/[stock]/page.tsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 
 // Lazy load
 const Chart = dynamic(() => import("./Chart"));
-const News = dynamic(() => import("../../news/page"));
 
 interface iQuote {
   c: number; // current price
@@ -46,7 +45,7 @@ export default function StockDetails({
               )}
             </section>
 
-            <h1 className="whitespace-nowrap text-3xl max-w-sm">{profile.isSuccess && profile.data.name || <br/>}</h1>
+            <h1 className="whitespace-nowrap text-3xl max-w-[300px] sm:max-w-full">{profile.isSuccess && profile.data.name || <br/>}</h1>
           </div>
 
           <div className="text-end">
@@ -73,7 +72,20 @@ export default function StockDetails({
 
       <div className="mt-6 text-neutral-100 transition-all">
         <h1 className="text-xl font-bold">Related News</h1>
-        <News />
+        <div className="mt-4 flex flex-col gap-3">
+          {[...Array(4)].map((_x, i) => (
+            <article
+              key={i}
+              className="flex h-28 cursor-pointer items-center gap-4 rounded-xl border border-neutral-800 p-2 hover:border-neutral-700"
+            >
+              <div className="h-full w-32 rounded-lg bg-white/[0.1]" />
+              <div className="flex h-full flex-col py-1">
+                <h2 className="text-lg font-bold">Story Title</h2>
+                <p className="text-sm text-neutral-400">Lorem Ipsum</p>
+              </div>
+            </article>
+          ))}
+        </div>
       </div>
     </>
   );
