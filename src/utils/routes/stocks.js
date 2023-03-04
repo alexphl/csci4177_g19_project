e073c8c0016e30c920b6f9beac74fc9f8f3031f7
Olexiy Prokhvatylo <olexiyprokhvatylo@gmail.com>
2023-03-04 13:01:51 -0400

    Stocks API: implement LRU cache for requests

--- src/utils/routes/stocks.js
+++ src/utils/routes/stocks.js
@@ -1,33 +1,66 @@
 "use strict";
 
-import { Router, json as _json } from "express";
+import { Router } from "express";
+import LRU from "lru-cache";
 const router = Router();
 
+const cache = new LRU({
+  max: 200, // max number of cached responses
+  ttl: 600000, // Response's Time to Live (ms)
+});
+
 // Get all stocks
-router.get("/", _json(), async function (req, res) {
+router.get("/", async function (req, res) {
+  const cached = cache.get(req.url);
+  if (cached) {
+    res.send(cached);
+    return;
+  }
+
   fetch(
     `https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${process.env.FINNHUB_API_KEY}`
   )
     .then((res) => res.json())
-    .then((json) => res.send(json));
+    .then((json) => {
+      cache.set(req.url, json);
+      res.send(json);
+    });
 });
 
 // Get quote for a stock
-router.get("/quote/:symbol", _json(), async function (req, res) {
+router.get("/quote/:symbol", async function (req, res) {
+  const cached = cache.get(req.url);
+  if (cached) {
+    res.send(cached);
+    return;
+  }
+
   fetch(
     `https://finnhub.io/api/v1/quote?symbol=${req.params.symbol}&token=${process.env.FINNHUB_API_KEY}`
   )
     .then((res) => res.json())
-    .then((json) => res.send(json));
+    .then((json) => {
+      cache.set(req.url, json);
+      res.send(json);
+    });
 });
 
 // Get quote for a stock
-router.get("/profile/:symbol", _json(), async function (req, res) {
+router.get("/profile/:symbol", async function (req, res) {
+  const cached = cache.get(req.url);
+  if (cached) {
+    res.send(cached);
+    return;
+  }
+
   fetch(
     `https://finnhub.io/api/v1/stock/profile2?symbol=${req.params.symbol}&token=${process.env.FINNHUB_API_KEY}`
   )
     .then((res) => res.json())
-    .then((json) => res.send(json));
+    .then((json) => {
+      cache.set(req.url, json);
+      res.send(json);
+    });
 });
 
 export default router;
