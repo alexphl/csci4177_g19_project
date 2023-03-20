43e065819f1b90817ccf9c50a9d0f14ae8040413
Olexiy Prokhvatylo <olexiyprokhvatylo@gmail.com>
2023-03-20 15:24:12 -0300

    Mobile nav: make functional

--- src/app/dashboard/NavMobile.tsx
+++ src/app/dashboard/NavMobile.tsx
@@ -13,13 +13,17 @@ const NavMobile = () => {
   const segment = useSelectedLayoutSegment();
 
   return (
-    <div className="fixed z-50 flex bottom-6 justify-center items-center w-full p-6 md:hidden">
-      <nav className="text-neutral-400 flex items-center justify-around bg-neutral-900/[0.8] border border-neutral-800 backdrop-blur-md shadow-lg w-full max-w-sm h-14 rounded-2xl">
+    <div className="fixed z-50 flex bottom-2 justify-center items-center w-full p-6 md:hidden">
+      <nav className="text-white/[0.7] flex items-center justify-around bg-neutral-900/[0.8] border border-neutral-800 backdrop-blur-md saturate-200 shadow-lg w-full max-w-sm h-14 rounded-2xl">
         {mobileBar.map((entry) =>
-          <div key={entry.link} className="w-10 p-2 hover:bg-white/[0.1] rounded-xl">
-            {entry.icon}
-          </div>)}
-        <div className="w-10 p-2 hover:bg-white/[0.1] rounded-xl">
+          <Link key={entry.link} href={entry.link}>
+            <div className="w-10 p-2 hover:bg-white/[0.05] rounded-xl saturate-200l">
+              {entry.icon}
+              {entry.heading.toLowerCase() === segment && <div className="absolute w-6 flex justify-center"><hr className="border w-3 rounded-full mt-3.5 border-white/[0.7]" /></div>}
+            </div>
+          </Link>
+        )}
+        <div className="w-10 p-2 hover:bg-white/[0.1] rounded-xl saturate-200">
           <Bars3Icon />
         </div>
       </nav>
