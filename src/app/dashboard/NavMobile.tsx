56e77a0f198fc1395dddd7dd19f46cb3dc38eead
Olexiy Prokhvatylo <olexiyprokhvatylo@gmail.com>
2023-03-20 14:58:18 -0300

    Fix build

--- src/app/dashboard/NavMobile.tsx
+++ src/app/dashboard/NavMobile.tsx
@@ -16,7 +16,7 @@ const NavMobile = () => {
     <div className="fixed z-50 flex bottom-6 justify-center items-center w-full p-6 md:hidden">
       <nav className="text-neutral-400 flex items-center justify-around bg-neutral-900/[0.8] border border-neutral-800 backdrop-blur-md shadow-lg w-full max-w-sm h-14 rounded-2xl">
         {mobileBar.map((entry) =>
-          <div className="w-10 p-2 hover:bg-white/[0.1] rounded-xl">
+          <div key={entry.link} className="w-10 p-2 hover:bg-white/[0.1] rounded-xl">
             {entry.icon}
           </div>)}
         <div className="w-10 p-2 hover:bg-white/[0.1] rounded-xl">
