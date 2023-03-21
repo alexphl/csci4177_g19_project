68b3c533d62b3e00da8c76f358219e8844b5a2d9
Olexiy Prokhvatylo <olexiyprokhvatylo@gmail.com>
2023-03-21 17:50:54 -0300

    Mobile nav: visual improvements

--- src/app/dashboard/NavMobile.tsx
+++ src/app/dashboard/NavMobile.tsx
@@ -13,17 +13,17 @@ const NavMobile = () => {
   const segment = useSelectedLayoutSegment();
 
   return (
-    <div className="fixed z-50 flex bottom-2 justify-center items-center w-full p-6 md:hidden">
-      <nav className="text-white/[0.7] flex items-center justify-around bg-neutral-900/[0.8] border border-neutral-800 backdrop-blur-md saturate-200 shadow-lg w-full max-w-sm h-14 rounded-2xl">
+    <div className="fixed z-50 flex bottom-5 justify-center items-center w-full px-8 md:hidden">
+      <nav className="text-white/[0.8] flex items-center justify-around bg-neutral-900/[0.6] border-[1.5px] border-white/[0.1] backdrop-blur-lg backdrop-saturate-[5.0] shadow-lg w-full max-w-sm h-[3.4rem] rounded-2xl">
         {mobileBar.map((entry) =>
           <Link key={entry.link} href={entry.link}>
-            <div className="w-10 p-2 hover:bg-white/[0.05] rounded-xl saturate-200l">
+            <div className="w-9 p-1.5 hover:bg-white/[0.05] rounded-xl drop-shadow-sm">
               {entry.icon}
-              {entry.heading.toLowerCase() === segment && <div className="absolute w-6 flex justify-center"><hr className="border w-3 rounded-full mt-3.5 border-white/[0.7]" /></div>}
+              {entry.heading.toLowerCase() === segment && <div className="absolute w-6 flex justify-center"><hr className="border w-3 rounded-full mt-3 border-white/[0.8]" /></div>}
             </div>
           </Link>
         )}
-        <div className="w-10 p-2 hover:bg-white/[0.1] rounded-xl saturate-200">
+        <div className="w-9 p-1.5 hover:bg-white/[0.1] rounded-xl saturate-200">
           <Bars3Icon />
         </div>
       </nav>
