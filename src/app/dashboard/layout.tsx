b97f77e289737faf3a65d8d6465218f3732c932a
Olexiy Prokhvatylo <olexiyprokhvatylo@gmail.com>
2023-03-22 19:53:01 -0300

    Add Dashboard metadata

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,14 @@
 import dynamic from "next/dynamic";
 import Transition from "./Transition";
+import type { Metadata } from "next";
 
 const NavDesktop = dynamic(() => import("./NavDesktop"));
 const NavMobile = dynamic(() => import("./NavMobile"));
+
+export const metadata: Metadata = {
+  title: "Dashboard - StockVision",
+  description: "Your personal dashboard",
+};
 
 export default function DashboardLayout({
   children,
